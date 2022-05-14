b354150f025596e602f63e7e52288fe177037d55
Tuananh15062001 <tuananh15062001@gmail.com>
2022-05-14 22:08:38 +0700

    14/05/2022

--- Lesson2_React/tiktok/src/components/Layout/DefaultLayout/Header/Header.jsx
+++ Lesson2_React/tiktok/src/components/Layout/DefaultLayout/Header/Header.jsx
@@ -1,27 +1,48 @@
-import './Header.scss';
-import images from '~/assets/images';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner} from '@fortawesome/free-solid-svg-icons'
+import Tippy from '@tippyjs/react/headless';
+
+import Propper from '~/components/Propper/Propper';
+import './Header.scss';
+import images from '~/assets/images';
+
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+    useEffect(() => {
+        setSearchResult([1, 2, 3])
+    }, [])
     return (
         <header className="wrapper">
             <div className="inner">
                 <div className="logo">
                     <img src={images.logo} alt="tiktok"/>
                 </div>
-                <div className='search'>
-                    <input placeholder='Search accounts and videos' spellCheck={false} />
-                    <button className='clear'>
-                        {/* clear */}
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    
-                    <FontAwesomeIcon className='loading' icon={faSpinner} />
-                    
-                    <button className='search-btn' >
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    </button>
-                </div>
+                <Tippy
+                    visible={searchResult.length > 0}
+                    interactive
+                    render={(attrs) => (
+                        <Propper>
+                            <div className='search-result' tabIndex="-1" {...attrs} >
+                                Ket qua
+                            </div>
+                        </Propper>
+                    )}
+                >
+                    <div className='search'>
+                        <input placeholder='Search accounts and videos' spellCheck={false} />
+                        <button className='clear'>
+                            {/* clear */}
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        
+                        <FontAwesomeIcon className='loading' icon={faSpinner} />
+    
+                        <button className='search-btn' >
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        </button>                
+                    </div>
+                </Tippy>
                 <div className='action'></div>
             </div>
         </header>
---
+++ Lesson2_React/tiktok/src/components/Propper/Propper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Propper.scss'
+function Propper({ children}) {
+  return (
+    <div className='propper'>{children}</div>
+  )
+}
+
+export default Propper
